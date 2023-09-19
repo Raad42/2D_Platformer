78b0af0a98619c586ba72bb12e6335150d333c07
Raad42 <raad.rahman@student.adelaide.edu.au>
2023-09-19 21:47:00 +0930

    made repo

---
+++ MarioGame.h
@@ -0,0 +1,15 @@
+#include <SFML/Graphics.hpp>
+#include <string>
+
+#ifndef MARIOGAME_H
+#define MARIOGAME_H
+
+//Basically runs the game window
+
+class MarioGame{
+    private:
+
+    public: 
+};
+
+#endif 
