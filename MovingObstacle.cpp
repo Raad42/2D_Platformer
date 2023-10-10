0baf8e5b2fdffc3ab0f3e9e09f708b65a674497e
Raad42 <raad.rahman@student.adelaide.edu.au>
2023-10-11 02:00:23 +1030

    added moving obstacle

--- MovingObstacle.cpp
+++ MovingObstacle.cpp
@@ -1,13 +1,32 @@
 #include "MovingObstacle.h"
+#include <SFML/Graphics.hpp>
 
-MovingObstacle::MovingObstacle(int x, int y, int width, int height)
-    : Obstacle(x, y, width, height) {
+MovingObstacle::MovingObstacle(int x, int y, int width, int height, sf::RenderWindow& window, float velocityXIn, float velocityYIn, float maxPosition, float minPosition)
+    : Obstacle(x, y, width, height, window), velocityX(velocityXIn), velocityY(velocityYIn), maxPosition(maxPosition), minPosition(minPosition){
+        moveUp = true;
     // Initialize moving obstacle-specific members here.
 }
 
 void MovingObstacle::update() {
-    // Specialized update logic for moving obstacles.
-    // Update position or behavior here.
-    // Call the base class's update function for common behavior.
-    Obstacle::update();
+    if (moveUp) {
+        // Move up until a certain point is reached (e.g., y < 100)
+        if ((get_y_position() > maxPosition && velocityX == 0) || (get_x_position() > maxPosition && velocityY == 0)) {
+            set_x_position(get_x_position() - velocityX); 
+            set_y_position(get_y_position() - velocityY);
+        } else {
+            // Change direction when the upper limit is reached
+            moveUp = false;
+        }
+    } else {
+        // Move down until another point is reached (e.g., y > 800)
+        if ((get_y_position() < minPosition && velocityX == 0) || (get_x_position() < minPosition && velocityY == 0)) {
+            set_x_position(get_x_position() + velocityX); 
+            set_y_position(get_y_position() + velocityY);
+        } else {
+            // Change direction when the lower limit is reached
+            moveUp = true;
+        }
+    }
+
+    getSprite().setPosition(get_x_position(), get_y_position());
 }
--- MovingObstacle.h
+++ MovingObstacle.h
@@ -5,10 +5,18 @@
 
 class MovingObstacle : public Obstacle {
 private:
+    float velocityX;
+    float velocityY;
+
+    float maxPosition; 
+    float minPosition; 
+    bool moveUp; 
+
+
     // Add moving obstacle-specific members here.
 public:
-    MovingObstacle(int x, int y, int width, int height);
-    virtual void update() override;
+    MovingObstacle(int x, int y, int width, int height, sf::RenderWindow& window, float velocityXIn, float velocityYIn, float maxPosition, float minPosition);
+    void update() override;
     // Additional attributes and methods for moving obstacles.
 };
 
